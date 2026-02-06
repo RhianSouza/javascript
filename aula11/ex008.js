dec592f7b6bc13f6eec4187003084a0e65ad2771
RhianSouza <rhiansouza92@gmail.com>
2026-02-05 22:11:05 -0300

    novos exercícios

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 600
+console.log(`Sua velocidade é de ${vel}km/h`)
+if (vel > 80){
+    console.log('MUTADO')
+}
+console.log('Dirija sempre com cinto de segurança!!')
+//CONDIÇÃO SIMPLES
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Brasil'
+console.log (`Se você nasceu no ${país}:`)
+if (país == 'Brasil'){
+    console.log('Você é Brasileiro')
+} else {
+    console.log('Você é estrangeiro')
+}
