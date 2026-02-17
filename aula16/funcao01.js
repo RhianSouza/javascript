cfe646a9911d4ad8f83287fc9903bb7726b800e1
RhianSouza <rhiansouza92@gmail.com>
2026-02-16 21:40:51 -0300

    Novos Exercícios js

---
+++ aula16/funcao01.js
@@ -0,0 +1,13 @@
+function parimp(n){
+    if (n % 2 == 0){
+        return 'Par'
+    } else {
+        return 'Ímpar'
+    }
+}
+    // --- MANEIRA 1
+//console.log(parimp(852))
+
+    // --- MANEIRA 2
+ let resultado = parimp(7)
+ console.log(resultado)
---
+++ aula16/funcao03.js
@@ -0,0 +1,6 @@
+let A = function(n){
+    return n*2
+}
+
+
+console.log(A(25))
