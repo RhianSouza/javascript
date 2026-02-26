3b434f110f6d737660f275d36f70493b424fd40e
RhianSouza <rhiansouza92@gmail.com>
2026-02-25 21:10:25 -0300

    Exercício js

--- ex/dasafio01/desafio02.js
+++ ex/dasafio01/desafio02.js
@@ -1,11 +1,39 @@
 const carrinho = { 
-   nome: ["arroz", "macarrão", "óleo"],
-   preco: [8, 2, 3],
-   quantidade: [1, 1, 2],
+   produto: [
+      {nome: "Arroz", preco: 16, quantidade: 1},
+      {nome: "Macarrão", preco: 12, quantidade: 3},
+      {nome: "Óleo", preco: 5, quantidade: 6},
+   ],
 
-   mostrarProdutos: function() {
-        console.log(`produto: ${this.nome[0]} Preço ${this.preco[0]} Quantidade ${this.quantidade[0]}`)
+   mostrarProdutos: function(){
+      for(let i = 0; i < this.produto.length; i++){
+         console.log(`Produto: ${this.produto[i].nome} Preço: ${this.produto[i].preco} Quantidade: ${this.produto[i].quantidade}`)
+      }
+   },
+
+   calcularTotal: function(){
+      let total = 0
+      for (let i = 0; i < this.produto.length; i++){
+
+         total += this.produto[i].preco * this.produto[i].quantidade
+      }
+      console.log(`O valor total da compra é R$${total}`)
+      },
+
+      adicionarProduto: function(nome, preco, quantidade){
+         this.produto.push({
+            nome: nome,
+            preco: preco,
+            quantidade: quantidad
+         })
+      }
+
+
    }
-}
+
+carrinho.adicionarProduto("leite", 4, 4)
+carrinho.adicionarProduto("Sal", 9, 1)
+carrinho.adicionarProduto("Açucar", 6, 2)
 
 carrinho.mostrarProdutos()
+carrinho.calcularTotal()
