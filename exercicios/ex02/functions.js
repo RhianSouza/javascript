8aa95620ba1b4dab47ea3e7f24a07de8f30b1dd5
RhianSouza <rhiansouza92@gmail.com>
2026-03-04 21:09:11 -0300

    exercícios funções

--- exercicios/ex02/functions.js
+++ exercicios/ex02/functions.js
@@ -1,11 +1,36 @@
-/*function sayMyName(){
-    console.log('rhian')
+// FUNÇÕES DECLARATION
+/*
+palindromo()
+
+function palindromo() { 
+    let palavra = "repaper"
+    let separandoLetras = palavra.split("")
+    let palavraIvertida = separandoLetras.reverse("")
+    palavraIvertida = palavraIvertida.join("")
+
+    if (palavra == palavraIvertida){
+        console.log("A palavra " + palavra + " É um palíndromo")
+    } else {
+        console.log("A palavra " + palavra + " NÃO é um palíndromo")
+    }
+}
+    */
+
+
+
+// FUNCÕES EXPRESSION
+
+const palindromo = function () {
+    let palavra = "genious"
+    let separandoLetras = palavra.split("")
+    let palavraIvertida = separandoLetras.reverse()
+    palavraIvertida = palavraIvertida.join("")
+
+    if (palavra == palavraIvertida) {
+        console.log("A palavra " + palavra + " É um palíndromo")
+    } else {
+        console.log("A palavra " + palavra + " NÃO é um palíndromo")
+    }
 }
 
-sayMyName()
-sayMyName()*/
-
-
-function dizUmNum(num) {
-    console.log(num)
-} dizUmNum('h')
+palindromo()
---
+++ exercicios/ex02/functions01.js
@@ -0,0 +1,25 @@
+// 1 ----------
+function dobro(num) {
+    return num * 2
+}
+
+console.log(dobro(28))
+
+// 2 ----------
+
+function soma(n1, n2){
+    return n1 + n2
+}
+
+console.log(soma(5, 3))
+
+// 3 ----------
+
+function media(a, b, c){
+    return (a + b + c) / 3
+}
+
+resultado = media(5, 9, 8)
+
+console.log(resultado.toFixed(2))
+// toFixed transforma o numero em string!!!!
