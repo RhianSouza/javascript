c78598d3792ee77d268961a2b2c8024e11cbd0d6
RhianSouza <rhiansouza92@gmail.com>
2026-03-24 19:26:12 -0300

    Novo desafio js

---
+++ exercicios/ex03/desafioarrays.js
@@ -0,0 +1,34 @@
+// EXERCÍCIO 1 / FEITO
+/*
+const carrinho = [
+    { nome: "Mouse", preco: 50, quantidade: 2 },
+    { nome: "Teclado", preco: 150, quantidade: 1 },
+    { nome: "Monitor", preco: 900, quantidade: 1 }
+];
+
+const nameProduct = carrinho.map(np => np.nome) 
+
+const higherPrice = carrinho.some(hp => hp.preco > 500)
+
+const highestZero = carrinho.every(hz => hz.quantidade > 0)
+
+const totalValue = carrinho.reduce((acc, t) => {
+    return acc + (t.preco * t.quantidade)
+}, 0)
+
+console.log(nameProduct)
+console.log(higherPrice)
+console.log(highestZero)
+console.log(totalValue)
+ */
+
+
+// 2 -----------------
+
+
+const usuarios = [
+  { nome: "Ana", idade: 22, ativo: true },
+  { nome: "Carlos", idade: 17, ativo: false },
+  { nome: "João", idade: 30, ativo: true },
+  { nome: "Marina", idade: 15, ativo: true }
+];
