00483a910e340eb0808d98ee6875f3c7c9840d0b
RhianSouza <rhiansouza92@gmail.com>
2026-05-05 22:16:09 -0300

    novos exercicios

---
+++ exercicios/ex07/fetch.js
@@ -0,0 +1,23 @@
+ fetch('https://jsonplaceholder.typicode.com/posts/1')
+    .then(response => response.json())
+    .then(objeto => {
+        console.log(objeto.title)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+ 
+//------------------------------------
+/* fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(data => {
+        const primeiros = f
+
+        primeiros.
+    })
+     
+    
+        CONTINUAR DEPOIS........
+
+
+    */
